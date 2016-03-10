1912019fdfda271c92b1baedcd2f1ab7a947a6a7
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-10 14:21:04 -0500

    updated
    updated the javascript to be more efficient and much shorter. Added
    puppy pics

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
-
-
 function changeColumns(numberOfColumns)	{
-	imageDivs = document.getElementsByClassName("images")
+	imageDivs = document.getElementsByClassName("images");
 	for(var i = 0; i < imageDivs.length; i++)	{
 		imageDivs[i].className = "images";
 		imageDivs[i].classList.add("col-lg-" + numberOfColumns);
 	}
 }
-
 function offset(numberOfOffset)	{
-	imageDivs = document.getElementsByClassName("images")
+	imageDivs = document.getElementsByClassName("images");
 	for(var i = 0; i < imageDivs.length; i++)	{
 		imageDivs[i].classList.add("col-lg-offset-" + numberOfOffset);
 	}
