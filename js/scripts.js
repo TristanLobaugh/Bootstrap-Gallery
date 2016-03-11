f375a993868b44cbbe4ed156a773b8a34e4f00c2
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-10 21:00:54 -0500

    Added more functionality
    Changed the list options to drop down menus using jQuery. Also fixed
    bug in js so you can adjust both the offsets and the columns at the
    same time.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
+var numOfColumns = "col-lg-3";
+var numOfOffset = "col-lg-offset-0";
+
 function changeColumns(numberOfColumns)	{
 	imageDivs = document.getElementsByClassName("images");
 	for(var i = 0; i < imageDivs.length; i++)	{
 		imageDivs[i].className = "images";
+		imageDivs[i].classList.add(numOfOffset);
 		imageDivs[i].classList.add("col-lg-" + numberOfColumns);
+		numOfColumns = "col-lg-" + numberOfColumns;
 	}
 }
 function offset(numberOfOffset)	{
 	imageDivs = document.getElementsByClassName("images");
 	for(var i = 0; i < imageDivs.length; i++)	{
+		imageDivs[i].className = "images";
+		imageDivs[i].classList.add(numOfColumns);
 		imageDivs[i].classList.add("col-lg-offset-" + numberOfOffset);
+		numOfOffset = "col-lg-offset-" + numberOfOffset;
 	}
 }
-
-
-
-
-
-
-
 
 
 
